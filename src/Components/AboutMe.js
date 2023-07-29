1f2b554a8f8418dded011c127a279c1235420a28
melika760 <72795928+melika760@users.noreply.github.com>
2023-07-29 16:41:05 +0330

    add skills

--- src/Components/AboutMe.js
+++ src/Components/AboutMe.js
@@ -1,7 +1,13 @@
 import React,{Fragment} from "react";
 import styles from "./AboutMe.module.css";
 import Mypicture from "../img/linkdin profile-finaly.jpg";
-import pdf from "../CV Melika Tafazoli.pdf"
+import pdf from "../CV Melika Tafazoli.pdf";
+import html from "../img/html.svg";
+import CSS from "../img/css.svg";
+import JS from "../img/javascript.svg";
+import Reactimg from "../img/react.svg";
+import Github from "../img/github.svg";
+import Firebase from "../img/firebase.svg"
 export default function AboutMe(){
     return(<Fragment>
         <div className={styles.wraper}>
@@ -17,14 +23,38 @@ other modern web technologies.</p>
 <img src={Mypicture} alt="Me"/>
 </div>
         </div>
+        <div className={styles.tool}>
         <div className={styles.content}>
-    <h3><span className={styles.shadow}></span>My Tools</h3>
-    <p>Highly skilled and goal-oriented Front-End Developer with a strong problem-solving ability and a desire to learn and 
-grow in a collaborative team environment. Graduated in Electrical Engineering, I have a proven track record of 
-creating user-friendly, responsive, and visually appealing websites. Proficient in HTML, CSS, JavaScript, React, and 
-other modern web technologies.</p>
+    <h3><span className={styles.shadow}></span>My Skills💻</h3>
+  <div className={styles.toolwraper}>
+  <div className={styles.symbol}>
+    <img src={html} alt="html element"/>
+    <p>HTML</p>
+   </div>
+   <div className={styles.symbol}>
+    <img src={CSS} alt="CSS element"/>
+    <p>CSS</p>
+   </div>
+   <div className={styles.symbol}>
+    <img src={JS} alt="JS element"/>
+    <p>JavaScript</p>
+   </div>
+   <div className={styles.symbol}>
+    <img src={Reactimg} alt="React element"/>
+    <p>React</p>
+   </div>
+   <div className={styles.symbol}>
+    <img src={Github} alt="GitHub element"/>
+    <p>GitHub</p>
+   </div>
+   <div className={styles.symbol}>
+    <img src={Firebase} alt="Firebase element"/>
+    <p>Firebase</p>
+   </div>
+  </div>
+   
 </div>
-
+</div>
         </Fragment>
     )
 }
