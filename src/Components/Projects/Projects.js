fe2b3b8c3690804bf19b7c97eaf8d653fd974ec8
melika760 <melikaaatafazoli@gmail.com>
2024-05-02 12:46:00 +0100

    changing some details

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -2,7 +2,7 @@
 import Modal from "./Modal";
 import styles from "./Projects.module.css";
 import Weather from "../../img/Weather.jpg";
-import Todo from "../../img/Todo.jpg";
+import Todo from "../../img/Untitled design (3).png";
 import Dictionary from "../../img/Dictionary.jpg";
 import FoodApp from "../../img/FoodAppjpg.jpg";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -27,10 +27,9 @@ export default function Projects(){
       
         <h4>hover over any project to learn more!</h4>
 <div className={styles.container}>
-<div className={styles.Dictionary}>
-<div className={styles.modal}><Modal title="Dictionary" massage="Proficiently developed a dictionary app using React, REST API integration, and Axios. The app enables users to search for word meanings and incorporates a voice feature for accurate pronunciation" onclick={DictionaryLink}/>
+<div className={styles.ToDo}> <div className={styles.modal}><Modal title="ToDoApp" massage="Expertly created a to-do list app using React, employing clean and efficient code. The app allows users to easily manage their tasks, add new items, mark them as completed, and remove them as needed." onclick={ToDoapp} />
 </div>
-<img src={Dictionary} alt="DictionaryApp"/>
+<img src={Todo} alt="ToDOApp"/>
 </div>
 <div className={styles.FoodApp}> <div className={styles.modal}><Modal title="FoodApp" massage=" Implemented real-time menu updates and user-friendly customization. Enhanced the overall user experience for smooth and efficient ordering"onclick={Foodapp}  />
 </div>
@@ -38,9 +37,10 @@ export default function Projects(){
 </div>
 </div>
 <div className={styles.container}>
-<div className={styles.ToDo}> <div className={styles.modal}><Modal title="ToDoApp" massage="Expertly created a to-do list app using React, employing clean and efficient code. The app allows users to easily manage their tasks, add new items, mark them as completed, and remove them as needed." onclick={ToDoapp} />
+<div className={styles.Dictionary}>
+<div className={styles.modal}><Modal title="Dictionary" massage="Proficiently developed a dictionary app using React, REST API integration, and Axios. The app enables users to search for word meanings and incorporates a voice feature for accurate pronunciation" onclick={DictionaryLink}/>
 </div>
-<img src={Todo} alt="ToDOApp"/>
+<img src={Dictionary} alt="DictionaryApp"/>
 </div>
 <div className={styles.Weather}>
  <div className={styles.modal}><Modal title="WeatherApp" massage="I built a weather app using React, REST API and Axios. The app allows users to track weather conditions in any location and provides a 5-day forecast. It also includes a dark mode feature for improved visibility." onclick={WeatherApp} />
